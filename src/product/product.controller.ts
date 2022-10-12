c4018b22168dae57b3da513602daa4f95aa7e44f
Aleksey <alekseytim@mail.ru>
2022-10-12 18:01:45 +0300

    feat: Mongo agregation

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,34 +1,55 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
-import { HttpCode, Body, Controller, Post, Get, Param, Delete, Patch } from '@nestjs/common';
+import { HttpCode, Body, Controller, Post, Get, Param, Delete, Patch, NotFoundException, UsePipes, ValidationPipe } from '@nestjs/common';
+import { NotFoundError } from 'rxjs';
+import { CreateProductDto } from './dto/create-product.dto';
 import { FindProductDto } from './dto/find-product.dto';
+import { PRODUCT_NOT_FOUND_ERROR } from './product.constants';
 import { ProductModel } from './product.model';
+import { ProductService } from './product.service';
 
 @Controller('product')
 export class ProductController {
 
-  @Post('create')
-  async create(@Body() dto: Omit<ProductModel, '_id'>) {
+  constructor(private readonly productService: ProductService) { }
 
+  @Post('create')
+  async create(@Body() dto: CreateProductDto) {
+    return this.productService.create(dto);
   }
 
   @Get(':id')
   async get(@Param('id') id: string) {
-
+    const product = await this.productService.findById(id);
+    if (!product) {
+      throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR)
+    }
+    return product;
   }
 
   @Delete(':id')
   async delete(@Param('id') id: string) {
-
+    const deletedProduct = await this.productService.deleteById(id);
+    if (!deletedProduct) {
+      throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR)
+    }
   }
 
   @Patch(':id')
   async patch(@Param('id') id: string, @Body() dto: ProductModel) {
-
+    const updatedProduct = await this.productService.updateById(id, dto);
+    if (!updatedProduct) {
+      throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR)
+    }
+    return updatedProduct;
   }
 
+
+  @UsePipes(new ValidationPipe())
   @HttpCode(200)
-  @Post('')
+  @Post('find')
   async find(@Body() dto: FindProductDto) {
-
+    return this.productService.findWithReviews(dto);
   }
+
+
 }
