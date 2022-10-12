82aa0d8cae376b9cbaac38b291d2c29d92eff091
Aleksey <alekseytim@mail.ru>
2022-10-12 12:14:00 +0300

    feat: JWT strategy and guard

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,4 +1,17 @@
-import { Controller, Post, Body, Delete, Param, Get, HttpException, HttpStatus, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Delete,
+  Param,
+  Get,
+  HttpException,
+  HttpStatus,
+  UsePipes,
+  ValidationPipe,
+  UseGuards
+} from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { ReviewService } from './review.service';
@@ -13,6 +26,7 @@ export class ReviewController {
     return this.reviewService.create(dto)
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   async delete(@Param('id') id: string) {
     const deletedDoc = await this.reviewService.delete(id);
@@ -20,6 +34,7 @@ export class ReviewController {
       throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND)
     }
   }
+
 
   @Get('byProduct/:productId')
   async getByProduct(@Param('productId') productId: string) {
